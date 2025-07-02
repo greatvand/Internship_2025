36c39437db6546609658470805e5963b3e50eb15
Utkarsh <utkarsh.agarwal@xebia.com>
2025-07-02 11:01:56 +0530

    Updated files

--- Monitoring/Project/index.js
+++ Monitoring/Project/index.js
@@ -7,26 +7,41 @@ const PORT = process.env.PORT || 8000;
 const client = require("prom-client");
 const collectDefaultMetrics = client.collectDefaultMetrics;
 
+const {createLogger, transports} = require("winston");
+const LokiTransport = require("winston-loki");
+
+const logger = createLogger({
+    transports: [
+        new LokiTransport({
+            host: "http://127.0.0.1:3100"
+        })
+    ]
+});
+
 collectDefaultMetrics({ register: client.register });
 
 app.get("/metrics", async (req, res) => {   
+    
     res.setHeader("Content-Type", client.register.contentType);
     const metrics = await client.register.metrics();
     res.send(metrics);
 });
 
 app.get("/", (req, res) => {
+    logger.info("Request received on / route");
   return res.json({ message: `Hello from Express Server` });
 });
 
 app.get("/slow", async (req, res) => {
   try {
+    logger.info("Request received on /slow route");
   const timeTaken = await doSomeHeavyTask();
   return res.json({
     status: "Success",
     message: `Heavy task completed in ${timeTaken}ms`,
   });
 } catch (error) {
+    logger.error("Error in /slow route", { error: error.message });
   return res
     .status(500)
     .json({ status: "Error", error: error.msg });
